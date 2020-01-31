07d8931b0135c43beb5fca97bb936fd7cdadb594
Alberto <albertogr578@gmail.com>
2020-01-31 16:19:27 +0100

    Añade mensaje por consola

---
+++ src/main/java/com/mycompany/ejercicioartifact/EjArtifact.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.ejercicioartifact;
+
+/**
+ *
+ * @author GR6
+ */
+public class EjArtifact {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo");
+    }
+    
+}
